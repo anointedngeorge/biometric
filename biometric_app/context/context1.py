78c8b8fa4c047a130d5d553d6614bf611cfcc572
anointedngeorge <anointedngeorge@gmail.com>
2023-08-07 07:18:47 +0100

    added student dashboard

--- biometric_app/context/context1.py
+++ biometric_app/context/context1.py
@@ -3,11 +3,14 @@ from authuser.forms import StudentForm, LecturerForm
 from frontend.models import *
 
 def processor(request):
+    
+
+
     return {
         'processor_subject':Subjects.objects.all(),
         'studentform':StudentForm,
         'lecturerform':LecturerForm,
         'sliders':Slider.objects.all(),
         'about':About.objects.all().first(),
-        'logo':Logo.objects.all().first(),
+        'logo_url':Logo.objects.all().first(),
     }
