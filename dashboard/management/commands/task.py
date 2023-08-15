bef74df205ad12498bea2f721bb66507b7e17989
anointedngeorge <anointedngeorge@gmail.com>
2023-08-15 08:33:17 +0100

    added a custom command

--- dashboard/management/commands/task.py
+++ dashboard/management/commands/task.py
@@ -1,10 +1,14 @@
 from django.core.management.base import BaseCommand
 import schedule
 import time
+from dashboard.models import TestTaskOnHeroku
+import datetime
 
 class Command(BaseCommand):
     help = 'Run a scheduled task'
 
     def handle(self, *args, **options):
         # Define your task function
-        return  "Hy loading"
+        tm =  datetime.datetime.now()
+        TestTaskOnHeroku.objects.create(data=f"Created {tm}")
+        return  'created'
